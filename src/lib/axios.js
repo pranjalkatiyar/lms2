e1cc91e48e304a19ba035f302597ff13351273c1
pranjalkatiyar <pranjalkatiyar23@gmail.com>
2025-04-28 03:32:07 +0530

    url udpate

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  // baseURL: "http://localhost:3001/api",
+  baseURL: "https://simviz-backend.onrender.com/api"
 });
 
 // Add a request interceptor to include auth token
